b0458be66c71574a5042444f9c2b09ea959718e2
julejoli <julejoli@jyu.fi>
2021-10-25 18:47:16 +0300

    Moved offset, multiplication, and slope to functions, added comments

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
+from os import path
+import os
+from pathlib import Path
+import numpy as np
+from matplotlib import pyplot as plt
+from astropy.io import fits
+from scipy import io
+import pandas as pd
+from solar import solar_irradiance
+
 
 def bb_radiance(T, eps, wavelength):
     """
     Calculate and return approximate thermal emission (blackbody, bb) radiance spectrum using Planck's law.
+
     :param T: float. Temperature in Kelvins
     :param eps: float. Emissivity = sample emission spectrum divided by ideal bb spectrum. Maybe in future accepts
     a vector, but only constants for now
     :param wavelength: vector of floats. Wavelengths where the emission is to be calculated, in micrometers.
+
     :return L_th: vector of floats. Spectral radiance emitted by the surface.
     """
 
@@ -17,7 +29,7 @@ def bb_radiance(T, eps, wavelength):
     L_th = np.zeros((len(wavelength),2))
     L_th[:,0] = wavelength
 
-    for i in range(0, len(wavelength)): # TODO: nollan voi jättää pois, se on defaultti
+    for i in range(len(wavelength)):
         wl = wavelength[i] / 1e6  # Convert wavelength from micrometers to meters
         L_th[i,1] = eps * (2 * h * c**2) / ((wl**5) * (np.exp((h * c)/(wl * kB * T)) - 1))  # Apply Planck's law
         L_th[i,1] = L_th[i,1] / 1e6  # Convert radiance from (W / m² / sr / m) to (W / m² / sr / µm)
@@ -25,21 +37,22 @@ def bb_radiance(T, eps, wavelength):
     return L_th
 
 
-def reflected_radiance(reflectance, irradiance, phi): # TODO: muuttujille voi laittaa myös vinkit niiden datatyypista esim. phi: float
+def reflected_radiance(reflectance: np.ndarray, irradiance: np.ndarray, phi: float):
     """
     Calculate spectral radiance reflected from a surface, based on the surface reflectance, irradiance incident on it,
     and the phase angle of the measurement. The surface normal is assumed to point towards the observer.
 
-    TODO: jätä rivinvaihto ennenku alotat parametrilistan
-    TODO: parametrilistasta tulee siistimpi kun laitat rivinvaihdon ennen selitystä esim.
-
     :param reflectance: vector of floats.
         Spectral reflectance.
+    :param reflectance: vector of floats.
+        Spectral reflectance.
+    :param irradiance: vector of floats.
+        Spectral irradiance incident on the surface.
+    :param phi: float.
+        Phase angle of the measurement, in degrees
 
-    :param reflectance: vector of floats. Spectral reflectance.
-    :param irradiance: vector of floats. Spectral irradiance incident on the surface.
-    :param phi: float. Phase angle of the measurement, in degrees
-    :return: vector of floats. Spectral radiance reflected toward the observer.
+    :return: vector of floats.
+        Spectral radiance reflected toward the observer.
     """
     # TODO Implement changing angle between surface normal and reflected light
     # TODO attach the wavelength vector to the returned data (and the inputs will have it also)
@@ -56,11 +69,15 @@ def read_meteorites(waves):
     """
     Load reflectance spectra from asteroid analogues measured by Maturilli et al. 2016 (DOI: 10.1186/s40623-016-0489-y)
     and from meteorite spectra measured by Gaffey in 1976 (https://doi.org/10.26033/4nsb-mc72)
-    :param waves: a vector of floats, wavelengths to which the data will be interpolated
-    :return: a list of Pandas DataArrays containing wavelength vectors and reflectance spectra
-    """ # TODO tapana jättää yks rivinvaihto metodikommentin jälkeen ennen ku koodi alkaa
-    # Path to folder of reflectance spectra from asteroid analogues
-    Maturilli_path = Path('./spectral_data/asteroid_analogues/refle/MIR') # TODO tää polkuluokka on mulle uus. ite oon käyttänu os.path
+
+    :param waves: a vector of floats
+        wavelengths to which the reflectance data will be interpolated
+
+    :return: a list of ndarrays containing wavelength vectors and reflectance spectra
+    """
+
+    # Path to folder of reflectance spectra from Maturilli's asteroid analogues
+    Maturilli_path = Path('./spectral_data/asteroid_analogues/refle/MIR')
     MIR_refl_list = os.listdir(Maturilli_path)
 
     # Path to folder of Gaffey meteorite spectra
@@ -71,28 +88,24 @@ def read_meteorites(waves):
 
     for filename in MIR_refl_list:
         filepath = Path.joinpath(Maturilli_path, filename)
-        # TODO tässä ois myös siistimpi käyttää sitä regexpii mikä kirjotettiin jonnekin. ihan siltäkin varalta että välilyöntejä ei satu olemaan just tuo määrä
-        frame = pd.read_csv(filepath, sep='    ', engine='python') # Read wl and reflectance from file
+        frame = pd.read_csv(filepath, sep=' +', header=None, names=('wavenumber', 'wl', 'reflectance'), engine='python') # Read wl and reflectance from file
         frame.columns = ['wavenumber', 'wl', 'reflectance']
         frame.drop('wavenumber', inplace=True, axis=1)  # Drop the wavenumbers, because who uses them anyway
-
-        # frame = frame.loc[frame['wl'] <= 2.5]  # Cut away wl:s above 2.5 µm. Also below 1.5 µm? Noisy.
 
         # Interpolate reflectance data to match the input wl-vector, and store into new dataFrame
         interp_refl = np.interp(waves, frame.wl.values, frame.reflectance.values)
         data = np.zeros((len(waves),2))
-        data[:,0] = waves
-        data[:,1] = interp_refl
-        frame = pd.DataFrame(data, columns=['wl', 'reflectance'])
+        data[:, 0] = waves
+        data[:, 1] = interp_refl
+        # frame = pd.DataFrame(data, columns=['wl', 'reflectance'])
 
-        reflectances.append(frame)
+        # reflectances.append(frame)
+        reflectances.append(data)
 
         # plt.figure()
         # plt.plot(frame['wl'], frame['reflectance'])
         # plt.show()
-        # plt.figure()
-        # plt.plot(range(0,len(frame['wl'])), frame['wl'],'*')
-        # plt.show()
+
     # TODO ja tosiaan tee tästä mieluummin apumetodi niin bugien korjaus onnistuu yhtä juttua muuttamalla
     for filename in Gaffey_refl_list:
         if filename.endswith('.tab'):
@@ -101,17 +114,15 @@ def read_meteorites(waves):
             frame.drop('error', inplace=True, axis=1)  # Drop the error -column, only a few have sensible data there
             frame.wl = frame.wl / 1000  # Convert nm to µm
 
-            # frame = frame.loc[frame['wl'] <= 2.5]  # Only include data from 1 µm to 2.5 µm
-            # frame = frame.loc[frame['wl'] >= 1.0]
-
             # Interpolate reflectance data to match the input wl-vector, and store into new dataFrame
             interp_refl = np.interp(waves, frame.wl.values, frame.reflectance.values)
             data = np.zeros((len(waves), 2))
             data[:, 0] = waves
             data[:, 1] = interp_refl
-            frame = pd.DataFrame(data, columns=['wl', 'reflectance'])
+            # frame = pd.DataFrame(data, columns=['wl', 'reflectance'])
 
-            reflectances.append(frame)
+            # reflectances.append(frame)
+            reflectances.append(data)
 
             # plt.figure()
             # plt.plot(frame['wl'], frame['R'])
@@ -121,34 +132,74 @@ def read_meteorites(waves):
 
     return reflectances
 
-def sloper(spectrum):
+
+def sloper(spectrum: np.ndarray):
+    """
+    Takes a spectrum as ndarray, and adds a slope to the reflectance part. The slopiness comes from a random number.
+
+    :param spectrum: ndarray
+        Reflectance spectrum, with wl vector in the first column, reflectance in the second
+
+    :return: ndarray
+        Sloped spectrum, in the same shape as the one given as parameter
+    """
+    val = (np.random.rand(1) - 0.5) * 0.1
+    slope = np.linspace(-val, val, len(spectrum)).flatten()
+    sloped = spectrum.copy()
+    sloped[:, 1] = sloped[:, 1] + slope
+    return sloped
+
+
+def multiplier(spectrum: np.ndarray):
+    """
+    Takes a spectrum as ndarray, and multiplies it by a random number between 0.5 and 1.5
+
+    :param spectrum: ndarray
+        Reflectance spectrum, with wl vector in the first column, reflectance in the second
+    :return: ndarray
+        Multiplied spectrum, in the same shape as the one given as parameter
+    """
+    val = np.random.rand(1) + 0.5
+    multiplied = spectrum.copy()
+    multiplied[:, 1] = multiplied[:, 1] * val
+    return multiplied
+
+
+def offsetter(spectrum: np.ndarray):
+    """
+    Takes a spectrum as ndarray, and offsets it by adding a random number between -0.1 and 0.1 to it
+
+    :param spectrum: ndarray
+        Reflectance spectrum, with wl vector in the first column, reflectance in the second
+    :return: ndarray
+        Offset spectrum, in the same shape as the one given as parameter
+    """
+    val = (np.random.rand(1) - 0.5) * 0.1
+    offsetted = spectrum.copy()
+    offsetted[:, 1] = offsetted[:, 1] + val
+    return offsetted
+
+def checker_fixer(spectrum: np.ndarray):
+    """
+    Check a reflectance spectrum for non-physical values: negative of greater than one. Fix negatives by offsetting,
+    and then too high values by normalizing.
+
+    :param spectrum: ndarray
+        Reflectance spectrum, with wl vector in the first column, reflectance in the second
+    :return: ndarray
+        Fixed spectrum, in the same shape as the one given as parameter
+    """
+    # If reflectance has negative values, offset by adding the minimum value to it
+    if min(spectrum[:, 1]) < 0:
+        spectrum[:, 1] = spectrum[:, 1] + min(spectrum[:, 1])
+
+    # If reflectance has values over 1, normalize by dividing each reflectance with the maximum
+    if max(spectrum[:, 1]) > 1:
+        spectrum[:, 1] = spectrum[:, 1] / max(spectrum[:, 1])
 
     return spectrum
 
-def multiplier(spectrum):
-    val = np.random.rand(1) + 0.5
-    multiplied = spectrum * val
-    return multiplied
-
-def offsetter(spectrum):
-    val = (np.random.rand(1) - 0.5) * 0.1
-    offsetted = spectrum + val
-    return offsetted
-
-# def checker_fixer(spectrum):
-
-
 if __name__ == '__main__':
-    # TODO impotit on yleensä tiedoston alussa järjestyksessä 1. python sisäänrakennetut, 2. julkiset moduulit 3. ite tehdyt moduulit
-    import numpy as np
-    from os import path
-    import os
-    from matplotlib import pyplot as plt
-    from solar import solar_irradiance
-    from astropy.io import fits
-    from pathlib import Path
-    from scipy import io
-    import pandas as pd
 
     # # Accessing measurements of Ryugu by the Hayabusa2:
     # hdulist = fits.open('hyb2_nirs3_20180710_cal.fit')
@@ -164,40 +215,37 @@ if __name__ == '__main__':
     # plt.plot(mystery_spectra)
     # plt.title('Ryugu')
     # plt.show()
-    step = 0.002
-    waves = np.arange(1, 2.5+step, step=step)  # Create wl-vector from 1 to 2.5 µm, with step given above
+
+    # Create wl-vector from 1 to 2.5 µm, with step size in µm
+    step = 0.002  # µm
+    waves = np.arange(1, 2.5+step, step=step)
 
     # Load meteorite reflectance spectra from files
     reflectance_spectra = read_meteorites(waves)
 
     # Augment reflectance spectra with slope, multiplication, and offset
-    #TODO First just one for testing, later do this for all
-    aug_number = 10  # How many new spectra to generate from each meteorite spectrum
-    # augmentation_factors = np.zeros((aug_number, 3)) # np.zeros((len(waves), 3))
+    aug_number = 10 # How many new spectra to generate from each meteorite spectrum
 
-    spectrum = reflectance_spectra[0].values
+    # spectrum = reflectance_spectra[100]
+    for j in range(len(reflectance_spectra)):
+        spectrum = reflectance_spectra[j]
+        for i in range(0, aug_number):
 
-    for i in range(0, aug_number):
-        vals = np.random.rand(3)
-        s = (vals[0] - 0.5) * 0.1  # Slope
-        m = vals[1] + 0.5  # Multiplication
-        o = (vals[2] - 0.5) * 0.1  # Offset
-        # augmentation_factors[i, :] = [s, m, o]
-        spectrum_multiplied = spectrum.copy()
-        spectrum_multiplied[:, 1] = spectrum_multiplied[:, 1] * m
+            spectrum_multiplied = multiplier(spectrum)
+            spectrum_multiplied_offset = offsetter(spectrum_multiplied)
+            spectrum_multiplied_offset_sloped = sloper(spectrum_multiplied_offset)
+            spectrum_multiplied_offset_sloped = checker_fixer(spectrum_multiplied_offset_sloped)
 
-        spectrum_multiplied_offset = spectrum_multiplied.copy()
-        spectrum_multiplied_offset[:, 1] = spectrum_multiplied_offset[:, 1] + o
-
-        spectrum_sloped = spectrum_multiplied_offset.copy()
-
-        plt.figure()
-        plt.plot(spectrum[:,0], spectrum[:,1])
-        plt.plot(spectrum_multiplied[:, 0], spectrum_multiplied[:, 1])
-        plt.plot(spectrum_multiplied_offset[:, 0], spectrum_multiplied_offset[:, 1])
-        plt.show()
+            reflectance_spectra.append(spectrum_multiplied_offset_sloped)
+            # plt.figure()
+            # plt.plot(spectrum[:,0], spectrum[:,1])
+            # plt.plot(spectrum_multiplied[:, 0], spectrum_multiplied[:, 1])
+            # plt.plot(spectrum_multiplied_offset[:, 0], spectrum_multiplied_offset[:, 1])
+            # plt.plot(spectrum_multiplied_offset_sloped[:, 0], spectrum_multiplied_offset_sloped[:, 1])
+            # plt.show()
 
     print('test')
+
     # Take wavelength vector of one reflectance spectrum, to be used for all the things
     # waves = reflectance_spectra[0]['wl']
 
@@ -210,16 +258,16 @@ if __name__ == '__main__':
     # reflectance[:, 1] = reflectance[:, 1] + 0.1
 
     # Interpolating insolation to match the reflectance chosen wavelength vector
-    interp_insolation = np.zeros(shape=np.shape(reflectance_spectra[0].values))
+    interp_insolation = np.zeros(shape=np.shape(reflectance_spectra[0]))
     interp_insolation[:, 0] = waves
     # TODO tämmöset kovakoodatut stringit tekee koodista vaikeeta muuttaa. tökkää toi wl nimi mieluummin muuttujaan ja käytä sitä
     # TODO mielellään vielä erilliseen filuun joka on sitten importattu kaikkialle missä niitä nimiä tarvitaan. voi tuntuu turhalta
     # TODO vielä ku koodi on nii lyhyt, mutta säästät hermoja myöhemmin
-    interp_insolation[:, 1] = np.interp(reflectance_spectra[0]['wl'], insolation_1au[:, 0], insolation_1au[:, 1])
+    interp_insolation[:, 1] = np.interp(waves, insolation_1au[:, 0], insolation_1au[:, 1])
 
     # Take one of the reflectance spectra and use it for calculating theoretical radiance reflected from an asteroid
-    spectrum_number = 2  # Which reflectance spectrum to use, from 0 to 6
-    reflectance = reflectance_spectra[spectrum_number].values
+    spectrum_number = 100  # Which reflectance spectrum to use, from 0 to 170
+    reflectance = reflectance_spectra[spectrum_number]
     phase_angle = 30  # degrees
     reflrad = reflected_radiance(reflectance, interp_insolation, phase_angle)
 
@@ -231,7 +279,7 @@ if __name__ == '__main__':
     figfolder = Path('./figs')
 
     plt.figure()
-    plt.plot(reflectance[:,0], reflectance[:,1])
+    plt.plot(reflectance[:, 0], reflectance[:, 1])
     plt.xlabel('Wavelength [µm]')
     plt.ylabel('Reflectance')
     figpath = Path.joinpath(figfolder , Path('reflectance.png'))
