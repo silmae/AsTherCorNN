Fixed Lommel-Seeliger calculation from Bond albedo to single-scattering. Still using L-S for all, even though it is unphysical for brighter samples.

radiance_data.py
@@ -51,10 +51,10 @@ def thermal_radiance(T: float, eps: np.ndarray, wavelength: np.ndarray):
 def reflected_radiance(reflectance: np.ndarray, irradiance: np.ndarray, incidence_angle: float, emission_angle: float):
     """
     Calculate spectral radiance reflected from a surface, based on the surface reflectance, irradiance incident on it,
-    and the phase angle of the measurement. Angle dependence is calculated using the Lommel-Seeliger model.
+    and the phase angle of the measurement. Angle dependence (BRDF) is calculated using the Lommel-Seeliger model.
 
     :param reflectance: vector of floats
-        Spectral reflectance.
+        Spectral reflectance, calculated using estimation for single-scattering albedo
     :param irradiance: vector of floats
         Spectral irradiance incident on the surface.
     :param incidence_angle: float
@@ -70,11 +70,11 @@ def reflected_radiance(reflectance: np.ndarray, irradiance: np.ndarray, incidenc
     reflrad = np.zeros((len(wavelength), 2))
     reflrad[:, 0] = wavelength
 
-    # Reflected radiance from flat surface with 0 phase angle
-    reflrad[:, 1] = irradiance[:, 1] * reflectance
+    # BRDF with Lommel-Seeliger
+    reflrad[:, 1] = (reflectance / (4 * np.pi)) * (1 / (np.cos(np.deg2rad(incidence_angle)) + np.cos(np.deg2rad(emission_angle))))
 
-    # Angle dependence with Lommel-Seeliger
-    reflrad[:, 1] = (reflrad[:, 1] / (4 * np.pi)) * (1 / (np.cos(np.deg2rad(incidence_angle)) + np.cos(np.deg2rad(emission_angle))))
+    # Reflected radiance from incident irradiance and BRDF
+    reflrad[:, 1] = irradiance[:, 1] * reflrad[:, 1]
 
     return reflrad
 
@@ -111,9 +111,7 @@ def noising(rad_data):
     Apply Gaussian noise to spectral data
 
     :param rad_data: ndarray
-
         Radiance data to which the noise will be applied: first column is wavelength, second is spectral radiance
-
     :return:
         Noisified radiance data
     """
@@ -238,7 +236,8 @@ def calculate_radiances(reflectance_list: list, test: bool, samples_per_temperat
         How many spectra will be generated per temperature value. Default is 200.
     :param constant_emissivity: Boolean
         Whether or not the thermal data generation will use constant 0.9 emissivity. Default is true, if false will use
-        Kirchhoff's law to generate emissivity spectrum from reflectance spectrum.
+        Kirchhoff's law to generate emissivity spectrum from reflectance spectrum. Note that Kirchhoff should only
+        be used when working with reflectances originally corrected using Kirchhoff.
 
     :return: summed, separate: ndarrays
         Arrays containing summed spectra and separate reflected and thermal spectra
@@ -247,7 +246,7 @@ def calculate_radiances(reflectance_list: list, test: bool, samples_per_temperat
 
     # Minimum temperature pulled from constants
     T_min = int(C.T_min)
-    # Maximum temperature as maximum possible for minimum heliocentric distance
+    # Maximum temperature as theoretical maximum at minimum heliocentric distance
     T_max = int(utils.calculate_subsolar_temperature(C.d_S_min))
 
     # Generate vector of temperatures from minimum to maximum with 1 K separation
@@ -262,7 +261,7 @@ def calculate_radiances(reflectance_list: list, test: bool, samples_per_temperat
 
     j = 0
 
-    # From each reflectance, create 10 radiances calculated with different parameters
+    # For each temperature
     for temperature in temperature_vector:
         for i in range(samples_per_temperature):
             # Create random variables from min-max ranges given in constants

reflectance_data.py
@@ -233,20 +233,19 @@ def scale_asteroid_reflectances(normalized_frame: pd.DataFrame, albedo_frame: pd
         alb = albedo_frame.loc[asteroid_class].values
         geom_albedos = np.array([alb[0] - 0.5*alb[1], alb[0], alb[0] + 0.5*alb[1]])
 
-        # Convert geometrical albedo to Bond albedo, assuming Lommel-Seeliger TODO Formula by Penttilä, find a reference or make it yoself
-        bond_albedos = 16 * geom_albedos * (1 - np.log(2)) / 3
-
         # Scale normalized reflectance with the three albedo values
         for i in range(3):
-            # Scale by multiplying with p/mean(norm_refl): values stay between 0 and 1, mean of scaled vector will be p
-            R = norm_reflectance * (bond_albedos[i] / np.mean(norm_reflectance))
+            # Un-normalize reflectance by scaling it with visual geometrical albedo
+            spectral_reflectance = norm_reflectance * geom_albedos[i]
+            # Convert reflectance to single-scattering albedo, using Lommel-Seeliger
+            spectral_single_scattering_albedo = 8 * spectral_reflectance
 
-            # Print if the physical limits of min and max reflectance are exceeded
-            if np.max(R) > 1 or np.min(R) < 0:
-                print(f'Unphysical reflectance detected! Max {np.max(R)}, min {np.min(R)}')
-            if np.mean(R) - bond_albedos[i] > 0.001:
-                print(f'Deviation from albedo detected! Difference between albedo and mean R {np.mean(R) - bond_albedos[i]}')
-            spectral_reflectances.append(R)
+            # Print if the physical limits of min and max reflectance are exceeded. And they will be, since L-S is not
+            # really suitable for bodies with geom. albedo > 0.125
+            if np.max(spectral_single_scattering_albedo) > 1 or np.min(spectral_single_scattering_albedo) < 0:
+                print(f'Unphysical reflectance detected! Max {np.max(spectral_single_scattering_albedo)}, min {np.min(spectral_single_scattering_albedo)}')
+
+            spectral_reflectances.append(spectral_single_scattering_albedo)
 
         # Plot every hundreth set of three reflectances, save plots to disc
         if plot_index % 100 == 0:

utils.py
@@ -102,21 +102,14 @@ def thermal_error_from_hc_distance(distance_min: float, distance_max: float, sam
             alb = albedo_frame.loc[asteroid_class].values
             geom_albedo = alb[0] - 0.5*alb[1]
 
-            # Convert geometrical albedo to Bond albedo, assuming Lommel-Seeliger TODO Formula by Penttilä, find a reference or make it yoself
-            bond_albedo = 16 * geom_albedo * (1 - np.log(2)) / 3
-
-            # Scale by multiplying with p/mean(norm_refl): values stay between 0 and 1, mean of scaled vector will be p
-            reflectance = norm_reflectance * (bond_albedo / np.mean(norm_reflectance))
+            # Un-normalize reflectance by scaling it with visual geometrical albedo
+            spectral_reflectance = norm_reflectance * geom_albedo
+            # Convert reflectance to single-scattering albedo, using Lommel-Seeliger
+            spectral_single_scattering_albedo = 8 * spectral_reflectance
 
             # Print if the physical limits of min and max reflectance are exceeded
-            if np.max(reflectance) > 1 or np.min(reflectance) < 0:
-                print(f'Unphysical reflectance detected! Max {np.max(reflectance)}, min {np.min(reflectance)}')
-            if np.mean(reflectance) - bond_albedo > 0.001:
-                print(f'Deviation from albedo detected! Difference between albedo and mean R {np.mean(reflectance) - bond_albedo}')
-            break
-
-    # Calculate spectral emittance with Kirchhoff's law
-    emittance = 1 - reflectance
+            if np.max(spectral_single_scattering_albedo) > 1 or np.min(spectral_single_scattering_albedo) < 0:
+                print(f'Unphysical reflectance detected! Max {np.max(spectral_single_scattering_albedo)}, min {np.min(spectral_single_scattering_albedo)}')
 
     # A list of heliocentric distances
     distances = np.linspace(distance_min, distance_max, samples)
@@ -132,8 +125,9 @@ def thermal_error_from_hc_distance(distance_min: float, distance_max: float, sam
                                               incidence_ang=0,
                                               emission_ang=0,
                                               T=temperatures[i],
-                                              reflectance=reflectance,
+                                              reflectance=spectral_single_scattering_albedo,
                                               waves=C.wavelengths,
+                                              constant_emissivity=True,
                                               filename='filename',
                                               test=True,
                                               save_file=False)

validation.py
@@ -12,6 +12,7 @@ import symfit
 import sympy
 
 import constants as C
+import file_handling
 import neural_network as NN
 import radiance_data as rad
 import file_handling as FH
@@ -631,9 +632,9 @@ def validate_bennu(model, validation_run_folder):
 
         fig = plt.figure()
         # Default pyplot colors: '#1f77b4', '#ff7f0e', '#2ca02c'
-        uncor_scatter1 =plt.scatter(ground_temps_1000, uncorrected_1000, alpha=0.1, color='#1f77b4')
-        uncor_scatter2 =plt.scatter(ground_temps_1230, uncorrected_1230, alpha=0.1, color='#1f77b4')
-        uncor_scatter3 =plt.scatter(ground_temps_1500, uncorrected_1500, alpha=0.1, color='#1f77b4')
+        uncor_scatter1 = plt.scatter(ground_temps_1000, uncorrected_1000, alpha=0.1, color='#1f77b4')
+        uncor_scatter2 = plt.scatter(ground_temps_1230, uncorrected_1230, alpha=0.1, color='#1f77b4')
+        uncor_scatter3 = plt.scatter(ground_temps_1500, uncorrected_1500, alpha=0.1, color='#1f77b4')
 
         cor_scatter1 = plt.scatter(ground_temps_1000, corrected_1000, alpha=0.1, color='#ff7f0e')
         cor_scatter2 = plt.scatter(ground_temps_1230, corrected_1230, alpha=0.1, color='#ff7f0e')
@@ -695,58 +696,76 @@ def error_plots(folderpath):
     MAE_dict = errordict['MAE']
     therm_MAE = MAE_dict['thermal_MAE']
     refl_MAE = MAE_dict['reflected_MAE']
+    refl_MAE_uncor = MAE_dict['reflected_MAE_uncorrected']
+    R_MAE = MAE_dict['reflectance_MAE']
+    R_MAE_uncor = MAE_dict['reflectance_MAE_uncorrected']
 
     SAM_dict = errordict['SAM']
     therm_SAM = SAM_dict['thermal_SAM']
     refl_SAM = SAM_dict['reflected_SAM']
+    refl_SAM_uncor = SAM_dict['reflected_SAM_uncorrected']
+    R_SAM = SAM_dict['reflectance_SAM']
+    R_SAM_uncor = SAM_dict['reflectance_SAM_uncorrected']
 
-    fig = plt.figure()
-    plt.figure()
-    plt.scatter(temperature_ground, temperature_error, alpha=0.1)
-    plt.xlabel('Ground truth temperature')
-    plt.ylabel('Temperature difference')
-    plt.savefig(Path(folderpath, 'tempdif_groundtemp.png'))
-    plt.close(fig)
+    def plot_and_save(data, label, filename):
+        fig = plt.figure()
+        plt.scatter(temperature_ground, data, alpha=0.1)
+        # plt.scatter(temperature_ground, R_MAE, alpha=0.1)
+        # plt.scatter(temperature_ground, R_MAE_uncor, alpha=0.1)
+        plt.xlabel('Ground truth temperature')
+        # plt.ylim(0,0.02)
+        plt.ylabel(label)
+        plt.savefig(Path(folderpath, filename))
+        plt.show()
+        plt.close(fig)
+    # plot_and_save(np.asarray(refl_MAE_uncor) - np.asarray(refl_MAE), r'MAE($L_{th}$) improvement', 'refl-MAE-improvement_groundtemp.png')
+    # plot_and_save(np.asarray(R_MAE_uncor) - np.asarray(R_MAE), r'MAE($R$) improvement', 'R-MAE-improvement_groundtemp.png')
 
-    fig = plt.figure()
-    plt.figure()
-    plt.scatter(temperature_ground, refl_SAM, alpha=0.1)
-    plt.xlabel('Ground truth temperature')
-    plt.ylabel('Reflected SAM')
-    plt.savefig(Path(folderpath, 'reflSAM_groundtemp.png'))
-    plt.close(fig)
-
-    fig = plt.figure()
-    plt.figure()
-    plt.scatter(temperature_ground, therm_SAM, alpha=0.1)
-    plt.xlabel('Ground truth temperature')
-    plt.ylabel('Thermal SAM')
-    plt.savefig(Path(folderpath, 'thermSAM_groundtemp.png'))
-    plt.close(fig)
-
-    fig = plt.figure()
-    plt.figure()
-    plt.scatter(temperature_ground, therm_MAE, alpha=0.1)
-    plt.xlabel('Ground truth temperature')
-    plt.ylabel('Thermal MAE')
-    plt.savefig(Path(folderpath, 'thermMAE_groundtemp.png'))
-    plt.close(fig)
-
-    fig = plt.figure()
-    plt.figure()
-    plt.scatter(temperature_ground, refl_MAE, alpha=0.1)
-    plt.xlabel('Ground truth temperature')
-    plt.ylabel('Reflected MAE')
-    plt.savefig(Path(folderpath, 'reflMAE_groundtemp.png'))
-    plt.close(fig)
+    plot_and_save(np.asarray(refl_SAM_uncor) - np.asarray(refl_SAM), r'SAM($L_{th}$) improvement', 'refl-SAM-improvement_groundtemp.png')
+    plot_and_save(np.asarray(R_SAM_uncor) - np.asarray(R_SAM), r'SAM($R$) improvement', 'R-SAM-improvement_groundtemp.png')
+    plot_and_save()
 
 
-def plot_Bennu_errors(errordict):
+    # fig = plt.figure()
+    # plt.figure()
+    # plt.scatter(temperature_ground, refl_SAM, alpha=0.1)
+    # plt.xlabel('Ground truth temperature')
+    # plt.ylabel('Reflected SAM')
+    # plt.savefig(Path(folderpath, 'reflSAM_groundtemp.png'))
+    # plt.close(fig)
+    #
+    # fig = plt.figure()
+    # plt.figure()
+    # plt.scatter(temperature_ground, therm_SAM, alpha=0.1)
+    # plt.xlabel('Ground truth temperature')
+    # plt.ylabel('Thermal SAM')
+    # plt.savefig(Path(folderpath, 'thermSAM_groundtemp.png'))
+    # plt.close(fig)
+    #
+    # fig = plt.figure()
+    # plt.figure()
+    # plt.scatter(temperature_ground, therm_MAE, alpha=0.1)
+    # plt.xlabel('Ground truth temperature')
+    # plt.ylabel('Thermal MAE')
+    # plt.savefig(Path(folderpath, 'thermMAE_groundtemp.png'))
+    # plt.close(fig)
+    #
+    # fig = plt.figure()
+    # plt.figure()
+    # plt.scatter(temperature_ground, refl_MAE, alpha=0.1)
+    # plt.xlabel('Ground truth temperature')
+    # plt.ylabel('Reflected MAE')
+    # plt.savefig(Path(folderpath, 'reflMAE_groundtemp.png'))
+    # plt.close(fig)
+
+
+def plot_Bennu_errors(folderpath):
+    errordict = file_handling.load_toml(Path(folderpath, 'errors_Bennu.toml'))
     errors_1000 = errordict['errors_1000']
     errors_1230 = errordict['errors_1230']
     errors_1500 = errordict['errors_1500']
 
-    def Bennuplot(errors_1000, errors_1230, errors_1500, data_name, label, savefolder):
+    def Bennuplot(errors_1000, errors_1230, errors_1500, data_name, label):
 
         def fetch_data(errordict, data_name):
             temperature_dict = errordict['temperature']
@@ -788,15 +807,70 @@ def plot_Bennu_errors(errordict):
             lh.set_alpha(1)
         if data_name == 'predicted_temperature':
             plt.plot(range(300, 350), range(300, 350), 'r')  # Plot a reference line with slope 1: ideal result
-        plt.savefig(Path(savefolder, f'{data_name}.png'))
+        plt.savefig(Path(folderpath, f'{data_name}.png'))
         # plt.show()
 
-    savefolder = C.val_and_test_path
-    Bennuplot(errors_1000, errors_1230, errors_1500, 'predicted_temperature', 'Predicted temperature [K]', savefolder)
-    Bennuplot(errors_1000, errors_1230, errors_1500, 'temperature_error', 'Temperature difference [K]', savefolder)
-    Bennuplot(errors_1000, errors_1230, errors_1500, 'reflected_MAE', 'Reflected MAE', savefolder)
-    Bennuplot(errors_1000, errors_1230, errors_1500, 'thermal_MAE', 'Thermal MAE', savefolder)
-    Bennuplot(errors_1000, errors_1230, errors_1500, 'reflected_SAM', 'Reflected SAM', savefolder)
-    Bennuplot(errors_1000, errors_1230, errors_1500, 'thermal_SAM', 'Thermal SAM', savefolder)
+    # savefolder = C.val_and_test_path
+    # Bennuplot(errors_1000, errors_1230, errors_1500, 'predicted_temperature', 'Predicted temperature [K]')
+    # Bennuplot(errors_1000, errors_1230, errors_1500, 'reflected_MAE', 'Reflected MAE')
+    # Bennuplot(errors_1000, errors_1230, errors_1500, 'thermal_MAE', 'Thermal MAE')
+    # Bennuplot(errors_1000, errors_1230, errors_1500, 'reflected_SAM', 'Reflected SAM')
+    # Bennuplot(errors_1000, errors_1230, errors_1500, 'thermal_SAM', 'Thermal SAM')
     print('test')
+
+    def Bennu_comparison_plots(corrected_name, uncorrected_name, label, lim=(0,0)):
+        temp_dict_1000 = errors_1000['temperature']
+        ground_temps_1000 = np.asarray(temp_dict_1000['ground_temperature'])
+        temp_dict_1230 = errors_1230['temperature']
+        ground_temps_1230 = np.asarray(temp_dict_1230['ground_temperature'])
+        temp_dict_1500 = errors_1500['temperature']
+        ground_temps_1500 = np.asarray(temp_dict_1500['ground_temperature'])
+
+        if 'MAE' in corrected_name:
+            dict_name = 'MAE'
+        elif 'SAM' in corrected_name:
+            dict_name = 'SAM'
+        dict_1000 = errors_1000[dict_name]
+        corrected_1000 = dict_1000[corrected_name]
+        uncorrected_1000 = dict_1000[uncorrected_name]
+        dict_1230 = errors_1230[dict_name]
+        corrected_1230 = dict_1230[corrected_name]
+        uncorrected_1230 = dict_1230[uncorrected_name]
+        dict_1500 = errors_1500[dict_name]
+        corrected_1500 = dict_1500[corrected_name]
+        uncorrected_1500 = dict_1500[uncorrected_name]
+
+        fig = plt.figure()
+
+        # cor_scatter1 = plt.scatter(ground_temps_1000, uncorrected_1000, alpha=0.1, color='r', marker='x')#'#ff7f0e')
+        # cor_scatter2 = plt.scatter(ground_temps_1230, uncorrected_1230, alpha=0.1, color='r', marker='x')#'#ff7f0e')
+        # cor_scatter3 = plt.scatter(ground_temps_1500, uncorrected_1500, alpha=0.1, color='r', marker='x')#'#ff7f0e')
+        # Default pyplot colors: '#1f77b4', '#ff7f0e', '#2ca02c'
+        # uncor_scatter1 = plt.scatter(ground_temps_1000, np.asarray(uncorrected_1000) - np.asarray(corrected_1000), alpha=0.1)#, color='#1f77b4')
+        # uncor_scatter2 = plt.scatter(ground_temps_1230, np.asarray(uncorrected_1230) - np.asarray(corrected_1230), alpha=0.1)#, color='#1f77b4')
+        # uncor_scatter3 = plt.scatter(ground_temps_1500, np.asarray(uncorrected_1500) - np.asarray(corrected_1500), alpha=0.1)#, color='#1f77b4')
+        uncor_scatter1 = plt.scatter(ground_temps_1000, np.asarray(uncorrected_1000) - np.asarray(corrected_1000),
+                                     alpha=0.1)  # , color='#1f77b4')
+        uncor_scatter2 = plt.scatter(ground_temps_1230, np.asarray(uncorrected_1230) - np.asarray(corrected_1230),
+                                     alpha=0.1)  # , color='#1f77b4')
+        uncor_scatter3 = plt.scatter(ground_temps_1500, np.asarray(uncorrected_1500) - np.asarray(corrected_1500),
+                                     alpha=0.1)  # , color='#1f77b4')
+
+
+        if lim != (0, 0):
+            plt.ylim(lim)
+
+        leg = plt.legend(('10:00', '12:30', '15:00'), title='Local time on Bennu')
+        for lh in leg.legendHandles:
+            lh.set_alpha(1)
+        plt.xlabel('Ground truth temperature [K]')
+        plt.ylabel(label)
+        # plt.show()
+        plt.savefig(Path(folderpath, f'{corrected_name}_improvement.png'))
+        plt.close(fig)
+
+    Bennu_comparison_plots('reflected_MAE', 'reflected_MAE_uncorrected', r'MAE($L_{refl}$) improvement)', lim=(-0.002, 0.004))
+    Bennu_comparison_plots('reflected_SAM', 'reflected_SAM_uncorrected', r'$L_{refl}$ cosine distance improvement', lim=(0, 0))
+    Bennu_comparison_plots('reflectance_MAE', 'reflectance_MAE_uncorrected', r'MAE($R$) improvement', lim=(-0.007, 0.009))
+    Bennu_comparison_plots('reflectance_SAM', 'reflectance_SAM_uncorrected', r'$R$ cosine distance improvement', lim=(0, 0))
 